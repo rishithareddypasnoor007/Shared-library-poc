0d0ff166d29bef03757008144f6470196f24bcf9
rishithareddypasnoor007 <55736650+rishithareddypasnoor007@users.noreply.github.com>
2021-02-03 15:56:48 -0500

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,7 +1,8 @@
 def gitLogin(String repo,String branch) {
 	git url: "https://github.com/rishithareddypasnoor007/$repo", branch: "$branch"
+	sh "cd ./$repo"
 }
 def build(String tag) {
-	sh "docker build -t "${tag}" ."
+	sh "docker build -t $tag ."
 }
 
