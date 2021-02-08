690832df2d3a598d1b4e49a167fc6cfac24ff020
rishithareddypasnoor007 <55736650+rishithareddypasnoor007@users.noreply.github.com>
2021-02-08 15:28:42 -0500

    Update gitLogin.groovy

--- vars/gitLogin.groovy
+++ vars/gitLogin.groovy
@@ -1,3 +1,3 @@
 def login(String branch, String credentials) {
-	git url: "https://github.com/Cowbell-Cyber/dairy.git", branch: "$branch", credentialsID: "$credentials"
+	git url: "https://github.com/Cowbell-Cyber/dairy.git", branch: "$branch", credentialsId: "$credentials"
 }
