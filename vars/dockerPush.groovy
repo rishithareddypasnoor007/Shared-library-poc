2b87bce21de696e61a2fdcb46507916a0a13d85d
rishithareddypasnoor007 <55736650+rishithareddypasnoor007@users.noreply.github.com>
2021-02-08 16:11:41 -0500

    Update dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,5 +1,4 @@
 def push(String awsRepoURL, String awsCredentials, String tag, String image){
-        docker.withRegistry('$URL', '$credentials'){
         docker.withRegistry('$awsRepoURL', '$awsCredentials'){
                 $image.push('$tag')
         }
