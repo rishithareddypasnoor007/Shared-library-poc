40975041e5fd8eba607e6ecf0f0ea505566ea74f
satishcb <61124384+satishcb@users.noreply.github.com>
2021-02-09 21:09:23 -0800

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -2,3 +2,9 @@ def build(String tag, String environment, String path) {
 	docker.build("$tag", "--build-arg DEPLOY_AS=$environment -f ${env.WORKSPACE}/$path/Dockerfile .")
 }
 
+def push(String awsRepoURL, String awsCredentials, String tag, Image image){
+       docker.withRegistry('$awsRepoURL', '$awsCredentials'){
+               image.push('$tag')
+       }
+}
+
