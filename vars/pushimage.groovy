45664ad9ab224c6433d7f64cbfedadc60865d05c
rishithareddypasnoor007 <55736650+rishithareddypasnoor007@users.noreply.github.com>
2021-02-10 10:45:19 -0500

    Create pushimage.groovy

---
+++ vars/pushimage.groovy
@@ -0,0 +1,5 @@
+def push(String awsRepoURL, String awsCredentials, String tag, Image image){
+       docker.withRegistry('$awsRepoURL', '$awsCredentials'){
+               image.push('$tag')
+       }
+}
