e9c0739609341c006ab2ae7301bc5b4e0be44cd8
rishithareddypasnoor007 <55736650+rishithareddypasnoor007@users.noreply.github.com>
2021-02-10 17:37:12 -0500

    Delete pushimage.groovy

--- vars/pushimage.groovy
+++
@@ -1,6 +0,0 @@
-def push(String awsRepoURL, String awsCredentials, String tag, String image){
-       print "$tag"
-       docker.withRegistry("$awsRepoURL", "$awsCredentials"){
-               image.push("$tag")
-       }
-}
